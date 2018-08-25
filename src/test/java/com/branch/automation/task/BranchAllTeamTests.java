4597e717205c137278b4bd6c5bd88146c5eecd9a
Slava <Iaroslava_Kornachevska@epam.com>
2018-08-25 09:01:30 -0700

    Rename class, add test to check if names from deps are present in all tab

--- src/test/java/com/branch/automation/task/BranchAllTeamTests.java
+++ src/test/java/com/branch/automation/task/BranchAllTeamTests.java
@@ -21,8 +21,8 @@ import java.util.logging.Logger;
  * Tests the flow: search for url under test (branch.io), click that url
  * Scrolls the page down till found team link
  */
-public class BranchTeamTests {
-    private static final Logger LOGGER = Logger.getLogger(BranchTeamTests.class.getName());
+public class BranchAllTeamTests {
+    private static final Logger LOGGER = Logger.getLogger(BranchAllTeamTests.class.getName());
     private static WebDriver driver;
 
     /**
@@ -47,11 +47,17 @@ public class BranchTeamTests {
     @Test
     public void compareTotalEmployeesNamesTest() {
         BranchTeamPage teamPage = searchAndNavigateToTeamPage();
-        List<String> employeesNamesFromAllTab = teamPage.getEmployeesNamesFromAllTab();
-        List<String> employeesNamesFromOtherTab = teamPage.getEmployeesNamesFromOthersTabs();
+        Assert.assertEquals("The names of employees from All tab do not match with total names from departments",
+                teamPage.getEmployeesNamesFromAllTab(), teamPage.getEmployeesNamesFromOthersTabs());
+    }
 
-        Assert.assertEquals("The names of employees from All tab does not match with the names from other tabs",
-                employeesNamesFromAllTab, employeesNamesFromOtherTab);
+    @Test
+    public void namesFromDepartmentsArePresentInAllTest() {
+        BranchTeamPage teamPage = searchAndNavigateToTeamPage();
+        List<String> employeesNamesFromAllTab = teamPage.getEmployeesNamesFromAllTab();
+        List<String> employeesNamesFromDeps = teamPage.getEmployeesNamesFromOthersTabs();
+        Assert.assertTrue("Departments have extra names comparing to All tab",
+                verifyNamesFromDepArePresentInAll(employeesNamesFromAllTab, employeesNamesFromDeps));
     }
 
     @Test
@@ -84,26 +90,30 @@ public class BranchTeamTests {
     }
 
     /**
-     * Method loop through "all" array and add every name to the map with counter 1.
-     * Then loop though "others" array and increment the counter if names already exist in map.
-     * Finally check name's counters if any less than 2, it means the matching name wasn't found.
-     * Log the names without match.
-     * @param all - list of employees from all tab
-     * @param others - list of employees from others tabs
-     * @return {@link boolean} - returns true if all names are matching.
+     * Add names from all to map, then check if names from departments are present in the map.
+     * This test pass if map has extra names, but fails if departments have extra names
+     * Log extra names.
+     * @param namesFromAll - list of employees from all tab
+     * @param namesFromDeps - list of employees from others tabs
+     * @return {@link boolean} - returns true if names from departments are present in All tab
      */
-    private boolean verifyEmployeesMatching(List<String> all, List<String> others) {
+    private boolean verifyNamesFromDepArePresentInAll(List<String> namesFromAll, List<String> namesFromDeps) {
         Map<String,Integer> map = new HashMap<>();
-        boolean hasMatch = true;
-        for (String employeeName : all) {
-            if (!map.containsKey(employeeName)) map.put(employeeName, 1);
+        boolean hasMatch = false;
+        for (String name : namesFromAll) {
+            if (!map.containsKey(name)) map.put(name, 1);
         }
-        for (String employeeName : others) {
-            if (map.containsKey(employeeName)) map.put(employeeName, map.get(employeeName) + 1);
+        for (String name : namesFromDeps) {
+            if (map.containsKey(name)) {
+                hasMatch = true;
+                map.put(name, map.get(name) + 1);
+            }
+            else {
+                hasMatch = false;
+            }
         }
         for (String employee : map.keySet()) {
             if (map.get(employee) < 2) {
-                hasMatch = false;
                 LOGGER.info("The employees was not found: " + employee);
             }
         }
